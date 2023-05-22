5734aeb625d8a39bc7710b1089060aff265aaa27
Anthony Hou <tonyhou@live.com>
2023-05-21 18:08:32 -0700

    Updated allowed hosts

--- lifepalbackend/settings.py
+++ lifepalbackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-52%s(*##tdrziiq2gnec*zn57!)&5gprsuqk+fs1&#+(0c^e!=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
 
 
 # Application definition
