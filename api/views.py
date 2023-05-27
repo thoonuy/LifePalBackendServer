4b74b676b151e545327bdc184001eebe9a6537ce
Anthony Hou <tonyhou@live.com>
2023-05-26 21:52:37 -0700

    Updated foodrec to consider mealtime and not have dupes

--- api/views.py
+++ api/views.py
@@ -69,16 +69,62 @@ def recommendfood(request):
 
 @api_view(['GET'])
 def foodrec(request):
+    def getrec(mealtime, w, bf, aa, c):
+        # 2.a. get FoodItem objects
+        menu = list(FoodItem.objects.filter(mealtime=mealtime))
+        if not menu: return [] # return [] if there's nothing
+
+        # 2.b. convert to food_list
+        food_list = []
+        for i in menu:
+            if i.calories == None: continue
+            food = {
+                'type': str(i.category),
+                'name': str(i.name),
+                'calories': float(i.calories)
+            }
+            food_list.append(food)
+
+        # 2.c. create FoodRecommendation object
+        fr = food_recommend.FoodRecommendation(food_list)
+
+        # 2.d. recommend 
+        recfoods = fr.recommend_foods(weight=w, fat=bf, avg_ae=aa, time=mealtime, c=c)
+        return [t[1] for t in recfoods]
+
+    def getFIObjects(meal):
+        fiobjs = [FoodItem.objects.get(name=fi) for fi in meal]
+        return fiobjs
+
     if request.method == 'GET':
         # 1. get params from request
-        # 2. for each meal time
-        # 2.a. get FoodItem objects
-        # 2.b. convert to food_list
-        # 2.c. create FoodRecommendation object
-        # 2.d. recommend 
-        # 2.e. reformat for response
+        weight = float(request.GET['weight'])
+        bodyfat = float(request.GET['bodyfat'])
+        avg_activity = float(request.GET['avg_activity'])
+        cb = int(request.GET['cb'])
+        cl = int(request.GET['cl'])
+        cd = int(request.GET['cd'])
 
-        responsedict = [{"under": "construction"}]
+        # 2. for each meal time
+        # 2.a. get recommendation
+        breakfast = getrec("breakfast", weight, bodyfat, avg_activity, cb)
+        lunch = getrec("lunch", weight, bodyfat, avg_activity, cl)
+        dinner = getrec("dinner", weight, bodyfat, avg_activity, cd)
+
+        # 2.b. reformat for response
+        breakfast = getFIObjects(breakfast)
+        lunch = getFIObjects(lunch)
+        dinner = getFIObjects(dinner)
+        
+        responsedict = [
+            {"category": "Breakfast",
+             "items": [x.reformatforios()[1] for x in breakfast]},
+            {"category": "Lunch",
+             "items": [x.reformatforios()[1] for x in lunch]},
+            {"category": "Dinner",
+             "items": [x.reformatforios()[1] for x in dinner]}
+        ]
+
         return Response(responsedict)
 
 @api_view(['GET'])
